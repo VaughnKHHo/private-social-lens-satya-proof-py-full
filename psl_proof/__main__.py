bd50517ba66c710b98496e7817c2756cbe159927
VaughnMYA <vaughn@mysoft.co.za>
2025-02-12 17:37:35 +0200

    url

--- psl_proof/__main__.py
+++ psl_proof/__main__.py
@@ -18,7 +18,7 @@ def load_config() -> Dict[str, Any]:
         'dlp_id': 4,
         'input_dir': INPUT_DIR,
         'salt': '5EkntCWI',
-        'validator_base_api_url': 'https://4073-209-203-23-196.ngrok-free.app'
+        'validator_base_api_url': 'https://3fad-209-203-23-196.ngrok-free.app'
     }
     logging.info(f"Using config: {json.dumps(config, indent=2)}")
     return config
